c3a10c4878b25d0a399fa7206e73c91d3f683b1b
mike seibel <mikes@washington.edu>
2020-09-01 13:15:35 -0700

    bump django-blti

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Django>=2.1,<2.2',
-        'django-blti>=2.1.11',
+        'django-blti>=2.2.0',
         'django-compressor',
         'python-memcached',
     ],
