bdd9229b2d0c62df09134453e8d22414cde65415
mike seibel <mikes@washington.edu>
2020-09-09 17:55:51 -0700

    complex lti config

--- docker/settings.py
+++ docker/settings.py
@@ -38,28 +38,137 @@ DETECT_USER_AGENTS = {
 }
 
 CANVAS_EXTERNAL_TOOLS = {
-    'uw_groups': {
-        'ext_id': '31485'
-    },
-    'uw_grade_scale': {
-        'ext_id': '13050',
-    },
-    'uw_classlist': {
-        'ext_id': '37913',
-        'subaccounts': ['uwcourse:']
-    },
-    'uw_accessibility': {
-        'ext_id': '37913',
-        'subaccounts': ['uwcourse:']
-    },
-    'uw_coda': {
-        'href_spec': 'https://coda.uw.edu/#{course_sis_id}',
-        'subaccounts': ['uwcourse:']
-    },
-    'myuw_classlist': {
-        'href_spec': 'https://my.uw.edu/teaching/{course_sws_id}/students',
-        'subaccounts': ['uwcourse:']
-    }
+    'uw_groups': [
+        {
+            'ext_id': '31485'
+        }
+    ],
+    'uw_grade_scale': [
+        {
+            'ext_id': '13050',
+        }
+    ],
+    'uw_classlist': [
+        {
+            'ext_id': '37913',
+            'subaccounts': ['uwcourse:']
+        }
+    ],
+    'uw_panopto': [
+        {
+            'ext_id': '21130',
+        }
+    ],
+    'uw_panopto_scheduler': [
+        {
+            'ext_id': '25694',
+            'subaccounts': ['uwcourse:seattle:arts-&-sciences:']
+        },
+        {
+            'ext_id': '25697',
+            'subaccounts': ['uwcourse:seattle:education:']
+        },
+        {
+            'ext_id': '25698',
+            'subaccounts': ['uwcourse:seattle:education:']
+        },
+        {
+            'ext_id': '25700',
+            'subaccounts': ['uwcourse:seattle:information-school:']
+        },
+        {
+            'ext_id': '25705',
+            'subaccounts': ['uwcourse:seattle:medicine:']
+        },
+        {
+            'ext_id': '25708',
+            'subaccounts': ['uwcourse:seattle:public-health:']
+        },
+        {
+            'ext_id': '25703',
+            'subaccounts': ['uwcourse:seattle:interschool-or-col:']
+        },
+        {
+            'ext_id': '25699',
+            'subaccounts': ['uwcourse:seattle:environment:']
+        },
+        {
+            'ext_id': '23909',
+            'subaccounts': ['uwcourse:seattle:business-school:']
+        },
+        {
+            'ext_id': '25706',
+            'subaccounts': ['uwcourse:seattle:nursing:']
+        },
+        {
+            'ext_id': '25707',
+            'subaccounts': ['uwcourse:seattle:pharmacy:']
+        },
+        {
+            'ext_id': '25695',
+            'subaccounts': ['uwcourse:seattle:built-environments:']
+        },
+        {
+            'ext_id': '25696',
+            'subaccounts': ['uwcourse:seattle:dentistry:']
+        },
+        {
+            'ext_id': '25702',
+            'subaccounts': ['uwcourse:seattle:interdisc-graduate-progr:']
+        },
+        {
+            'ext_id': '25704',
+            'subaccounts': ['uwcourse:seattle:law:']
+        },
+        {
+            'ext_id': '25710',
+            'subaccounts': ['uwcourse:seattle:social-work:']
+        },
+        {
+            'ext_id': '25701',
+            'subaccounts': ['uwcourse:seattle:int-col:']
+        },
+        {
+            'ext_id': '25709',
+            'subaccounts': ['uwcourse:seattle:pub-pol-&-gov:']
+        },
+        {
+            'ext_id': '44317',
+            'subaccounts': ['uwcourse:seattle:interdisc-undergrad-prog:train:']
+        }
+    ],
+    'uw_zoom': [
+        {
+            'ext_id': '83805',
+            'subaccounts': ['uwcourse:seattle:nursing:']
+        },
+        {
+            'ext_id': '95443',
+        },
+    ],
+    'uw_poll_everywhere': [
+        {
+            'ext_id': '41789',
+        }
+    ],
+    'uw_accessibility': [
+        {
+            'ext_id': '93468',
+            'subaccounts': ['uwcourse:']
+        }
+    ],
+    'uw_coda': [
+        {
+            'href_spec': 'https://coda.uw.edu/#{course_sis_id}',
+            'subaccounts': ['uwcourse:']
+        }
+    ],
+    'myuw_classlist': [
+        {
+            'href_spec': 'https://my.uw.edu/teaching/{course_sws_id}/students',
+            'subaccounts': ['uwcourse:']
+        }
+    ]
 }
 
 if os.getenv('ENV', 'localdev') == "localdev":
