20adda28fef152201992dad71a9120ed39e59646
Jim Laney <jlaney@uw.edu>
2021-02-26 16:38:37 -0800

    use github actions

--- conf/test.sh
+++ conf/test.sh
@@ -2,9 +2,8 @@
 set -e
 trap 'exit 1' ERR
 
-# travis test script for django app
 #
-# PRECONDITION: inherited env vars from application's .travis.yml MUST include:
+# PRECONDITION: inherited env vars from application MUST include:
 #      DJANGO_APP: django application directory name
 
 # start virtualenv
